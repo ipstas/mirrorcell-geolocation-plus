2d2ace58f9303b997ab24d64c05416de649acd95
Bryan Terrell <bryan.a.terrell@gmail.com>
2016-09-23 13:09:46 -0700

    Update references & rename package owner.

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
-    name: 'mirrorcell:geolocation-plus',
-    version: '1.1.8',
+    name: '37d:geolocation-plus',
+    version: '1.1.9',
     // Brief, one-line summary of the package.
     summary: 'A Geolocation Abstraction with Manual Starting / Stopping and location storage',
     // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/pmwisdom/mirrorcell-geolocation-plus',
+    git: 'https://github.com/37d/mirrorcell-geolocation-plus',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
 });
 
 Cordova.depends({
-    "org.apache.cordova.geolocation": "0.3.11",
+    "org.apache.cordova.geolocation": "2.3.0",
     "org.flybuy.nativeutils": "https://github.com/pmwisdom/NativeUtils/tarball/64460ce4e28a346245beb2c5a7413bd67a6c148d"
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.0.3.1');
+    api.versionsFrom('1.4');
 
     api.use(['session', 'reactive-var']);
 
