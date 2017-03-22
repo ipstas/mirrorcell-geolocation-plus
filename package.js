c8eb962e3c360a7a52e404f003b7c6fba866b04c
Bryan Terrell <bryan.a.terrell@gmail.com>
2017-03-22 12:54:53 -0700

    Update cordova geolocation plugin used.

--- package.js
+++ package.js
@@ -1,29 +1,29 @@
 Package.describe({
-    name: '37d:mirrorcell-geolocation-plus',
-    version: '1.1.9',
-    // Brief, one-line summary of the package.
-    summary: 'A Geolocation Abstraction with Manual Starting / Stopping and location storage',
-    // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/37d/mirrorcell-geolocation-plus',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+  name:          '37d:mirrorcell-geolocation-plus',
+  version:       '1.2.0',
+  // Brief, one-line summary of the package.
+  summary:       'A Geolocation Abstraction with Manual Starting / Stopping and location storage',
+  // URL to the Git repository containing the source code for this package.
+  git:           'https://github.com/37d/mirrorcell-geolocation-plus',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
 Cordova.depends({
-    "org.apache.cordova.geolocation": "2.3.0",
-    "org.flybuy.nativeutils": "https://github.com/pmwisdom/NativeUtils/tarball/64460ce4e28a346245beb2c5a7413bd67a6c148d"
+  "cordova-plugin-geolocation": "2.4.2",
+  "org.flybuy.nativeutils":     "https://github.com/pmwisdom/NativeUtils/tarball/64460ce4e28a346245beb2c5a7413bd67a6c148d"
 });
 
-Package.onUse(function(api) {
-    api.versionsFrom('1.4');
+Package.onUse(function (api) {
+  api.versionsFrom('1.4');
 
-    api.use(['session', 'reactive-var']);
+  api.use(['session', 'reactive-var']);
 
-    api.addFiles('lib/location.js');
+  api.addFiles('lib/location.js');
 
-    api.export('Location');
+  api.export('Location');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
 });
