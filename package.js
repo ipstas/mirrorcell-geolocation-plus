c2c05ef9d82d98bef5aef6db7232eff2c8641677
ipstas <stan@podolski.org>
2019-02-07 02:01:16 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,22 +1,21 @@
 Package.describe({
-  name:          '37d:mirrorcell-geolocation-plus',
-  version:       '1.2.1',
+  name:          'ipstas:mirrorcell-geolocation-plus',
+  version:       '1.2.2',
   // Brief, one-line summary of the package.
   summary:       'A Geolocation Abstraction with Manual Starting / Stopping and location storage',
   // URL to the Git repository containing the source code for this package.
-  git:           'https://github.com/37d/mirrorcell-geolocation-plus',
+  git:           'https://github.com/ipstas/mirrorcell-geolocation-plus',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Cordova.depends({
-  "cordova-plugin-geolocation": "2.4.3",
-  "org.flybuy.nativeutils":     "https://github.com/pmwisdom/NativeUtils/tarball/64460ce4e28a346245beb2c5a7413bd67a6c148d"
+  "cordova-plugin-geolocation": "4.0.1",
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.4');
+  api.versionsFrom('1.7');
 
   api.use(['session', 'reactive-var']);
 
